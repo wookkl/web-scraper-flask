81bc7c14abb56d8e25852ff961435a7474c53330
wookkl <57362359+wookkl@users.noreply.github.com>
2020-09-06 20:32:16 +0900

    Query Arguments

--- main.py
+++ main.py
@@ -1,16 +1,17 @@
-from flask import Flask
+from flask import Flask, render_template, request
 
 app = Flask("SuperScrapper")
 
 
 @app.route("/")
 def home():
-    return "Hello! Welcome to mi casa!"
+    return render_template("index.html")
 
 
-@app.route("/contact")
-def contact():
-    return "Contact me!"
+@app.route("/report")
+def report():
+    word = request.args.get('word')
+    return render_template("report.html",searchingBy = word)
 
 
 app.run(host="127.0.0.1")
