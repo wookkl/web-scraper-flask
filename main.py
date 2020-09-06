a13558822d79cd4d9e0b8f6afdbb433fab15a942
wookkl <57362359+wookkl@users.noreply.github.com>
2020-09-06 22:32:40 +0900

    Rendering Jobs

--- main.py
+++ main.py
@@ -1,6 +1,9 @@
-from flask import Flask, render_template, request
-
+from flask import Flask, render_template, request, redirect
+from scrapper import get_jobs
+from exporter import save_to_file
 app = Flask("SuperScrapper")
+
+db = {}
 
 
 @app.route("/")
@@ -11,7 +14,37 @@ def home():
 @app.route("/report")
 def report():
     word = request.args.get('word')
-    return render_template("report.html",searchingBy = word)
+    if word:
+        word = word.lower()
+        existingJobs = db.get(word)
+        if existingJobs:
+            jobs = existingJobs
+        else:
+            jobs = get_jobs(word)
+            db[word] = jobs
+    else:
+        return redirect("/")
+    return render_template(
+        "report.html",
+        searchingBy=word,
+        resultNumber=len(jobs),
+        jobs=jobs
+    )
+
+
+@app.route("/export")
+def export():
+    try:
+        word = request.args.get('word')
+        if not word:
+            raise Exception()
+        word = word.lower()
+        jobs = db.get(word)
+        if not jobs:
+            raise Exception()
+        return f"Generate CSV for {word}"
+    except:
+        return redirect("/")
 
 
 app.run(host="127.0.0.1")
